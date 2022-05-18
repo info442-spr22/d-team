25c23d5c5f717dd9d6351bfae53844ac0bf8b293
syesil00 <skylery00@gmail.com>
2022-05-17 21:00:54 -0700

    added card images

--- src/components/map.js
+++ src/components/map.js
@@ -85,6 +85,7 @@ export function MapScreen() {
     const LocationCard = currentPlaces.map((location, index) => {
         return (
             <div key={index} id={index} className="card" >
+                <img class="card-img-top" src={location.img} alt="Card image cap"></img>
                 <div className="card-body">
                     <h5 className="card-title">{location.name}</h5>
                     <h6 className="card-subtitle mb-2 text-muted">{location.address}</h6>
